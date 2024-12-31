bd3378afec264e8d6a18ec2c99f337eb7db95939
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2024-12-31 10:20:17 -0300

    Create user entity and role entity

---
+++ src/main/java/basicAuth/entity/UserEntity.java
@@ -0,0 +1,28 @@
+package basicAuth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "USERS")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<RoleEntity> roles;
+}
